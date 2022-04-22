87649ab2c5b296d450bc926ecd0d1f2be03000e2
Hakan Gok <hakangok1974@yahoo.com>
2022-04-22 11:04:43 +0100

    new line added

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,7 +2,9 @@ package hello;
 
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("found this project on gitHub - phew!");
 
     }
 }
